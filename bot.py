20e457ab3e29ee6b0e4b23844559e4a4b56a7137
dw2b <729301277@qq.com>
2024-01-31 00:13:48 +0800

    修复bug

--- bot.py
+++ bot.py
@@ -8,13 +8,14 @@ import config
 API_TOKEN=config.API_TOKEN
 information='使用 /new {date/future} [[time]] [[type]] [[content]] 指令创建一条新的提醒或定时任务.\n\n示例:\n\n*/new +3 8:00 去公园*\n_设置一条三天后上午八点的提醒, 内容是\"去公园\"_\n\n*/new 2024-2-10 19:00 send 吃饭*\n_设置一条于2024年2月10日傍晚七点的提醒, 内容是\"吃饭\"_\n\n*/new 23:30 睡觉*\n_设置一条于今日晚11点半的提醒, 内容是\"睡觉\"_\n\n*/new +1 7:00 do dnf update*\n_设置一条于明早七点执行的定时任务, 指令是\"dnf update\"_\n\n定时任务与提醒的设置格式相似, 只需在[[content]]前指定[[type]]为\"do\"即可, 若指定[[type]]为\"send\"或不指定, 则默认而选择创建一条新提醒而非定时任务.'
 proxy='-x 127.0.0.1:2080'
+tips='请使用 /help 指令查看说明.'
 
 bot=telebot.TeleBot(API_TOKEN,parse_mode='MARKDOWN')
 
 # /new [number/date/NOTHING] [time] [type] [content]
-# +3         8:00            send hello
-# 2024-1-30  8:00            send hello
-#            3:00            send hello
+# +3         8:00         send hello
+# 2024-1-30  8:00         send hello
+#         3:00         send hello
 
 def is_good_time(text):
     model=re.compile(r'^([01]?\d|2[0-3]):([0-5]?\d)$')
@@ -25,7 +26,7 @@ def is_good_date(text):
     return bool(model.match(text))
 
 def send_to(who,what):
-    os.system("curl -s %s -X POST \"https://api.telegram.org/bot%s/sendMessage\" -d \"chat_id=%s\" -d \"text=%s\" -d \"parse_mode=Markdown\" >> tmp"%(proxy,API_TOKEN,who,what))
+    os.system("curl -s %s -X POST \"https://api.telegram.org/bot%s/sendMessage\" -d \"chat_id=%s\" -d \"text=%s\" -d \"parse_mode=Markdown\" >> .tmp"%(proxy,API_TOKEN,who,what))
 
 def get_content(w):
     return ' '.join(w).replace('"','\\"').replace('send ','')
@@ -54,10 +55,10 @@ def analysis(word,who):
             else:
                 os.system('echo "curl %s -X POST \'https://api.telegram.org/bot%s/sendMessage\' -d \'chat_id=%s\' -d \'text=%s\' -d \'parse_mode=Markdown\'" | at %s + %s days'%(proxy,API_TOKEN,who,get_content(w[2:]),w[1],w[0].replace('+','')))
                 send_to(who,'已成功添加提醒👌 接收者: %s\n\n将于 *%s天后 - %s *发送: *%s*'%(who,w[0].replace('+',''),w[1],get_content(w[2:])))
-        else:
-            send_to(who,'格式错误😱\n%s'%information)
+        elif is_good_time(w[0])==False and is_good_date(w[0])==False and (w[0].startswith('+'))==False:
+            send_to(who,'格式错误😱\n\n%s'%tips)
     except IndexError:
-        send_to(who,'缺少参数🤯\n%s'%information)
+        send_to(who,'缺少参数🤯\n\n%s'%tips)
 
 @bot.message_handler(commands=['help','start'])
 def send_welcome(message):
